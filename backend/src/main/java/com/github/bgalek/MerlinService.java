e4c1503ce3c633fa27196db37b808ebdf4621d4c
Bartosz Galek <bartosz.galek@allegro.com>
2023-09-11 19:59:29 +0200

    additional log added

--- backend/src/main/java/com/github/bgalek/MerlinService.java
+++ backend/src/main/java/com/github/bgalek/MerlinService.java
@@ -3,10 +3,15 @@ package com.github.bgalek;
 import com.github.bgalek.levels.MerlinLevel;
 import com.theokanning.openai.service.OpenAiService;
 import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
 
+import java.time.Duration;
 import java.util.Optional;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class MerlinService {
+    private static final Logger logger = getLogger(MerlinService.class);
     private final OpenAiService openAiService;
     private final MerlinLevelRepository merlinLevelRepository;
 
@@ -37,6 +42,14 @@ public class MerlinService {
         int currentLevel = getCurrentLevel(httpSession);
         MerlinLevel merlinLevel = merlinLevelRepository.getLevel(currentLevel);
         httpSession.setAttribute("level", currentLevel + 1);
+        if (currentLevel == getMaxLevel()) {
+            httpSession.setAttribute("finished", true);
+            logger.info(
+                    "We have a winner! Session: {}, Duration: {}",
+                    httpSession.getId(),
+                    Duration.ofMillis(System.currentTimeMillis() - httpSession.getCreationTime()).toString()
+            );
+        }
         return merlinLevel.getLevelFinishedResponse();
     }
 
