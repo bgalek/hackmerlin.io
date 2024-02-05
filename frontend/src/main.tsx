cb45d66e4be6b46b5893aa85a5ed977213006cf3
Bartosz Galek <bartosz.galek@allegro.com>
2024-02-05 20:21:31 +0100

    added sentry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Notifications } from "@mantine/notifications";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ModalsProvider } from "@mantine/modals";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://938dcbf09e4bc4e05a089d6c36f830da@us.sentry.io/4506695788527616",
+  tracePropagationTargets: ["localhost", /^https:\/\/hackmerlin\.io\/api/],
+  integrations: [
+    new Sentry.BrowserTracing({}),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
