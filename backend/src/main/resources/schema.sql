994b5aad0b1b1961cb55cfc79d5ff11966ab1827
Bartosz Galek <bartosz@galek.com.pl>
2024-08-10 10:23:37 +0200

    logs and leadeboard restored

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -4,5 +4,14 @@ create table IF NOT EXISTS leaderboard
     session     char(36)  not null,
     name        char(200) not null,
     started_at  timestamp not null,
-    finished_at timestamp not null
+    finished_at timestamp default CURRENT_TIMESTAMP
+);
+
+create table IF NOT EXISTS logs
+(
+    id          serial primary key,
+    session     char(36)  not null,
+    level       int not null,
+    prompt      char(200) not null,
+    response    char(200) not null
 );
